3f7e1bc305482e759f4858ac0e81ee334e495684
Tildozer <actninswitch@gmail.com>
2023-07-12 15:38:02 -0600

    added resource examples

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,31 +5,29 @@ import viteLogo from "/vite.svg";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className=" bg-slate-700 text-4xl flex justify-between p-10">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-xl bg- bg-yellow-200">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/routes/App.jsx</code> and save to test HMR
+      <div className=" bg-slate-700 text-xl text-white">
+        <div className="flex justify-center items-center">
+          <a className="p-4" href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo}  alt="Vite logo" />
+          </a>
+          <a className="p-4" href="https://react.dev" target="_blank">
+            <img src={reactLogo}  alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div >
+          <p>
+            Edit <code>src/routes/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p >
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <canvas class="webgl"></canvas>
+      <canvas className="webgl"></canvas>
     </>
   );
 }
