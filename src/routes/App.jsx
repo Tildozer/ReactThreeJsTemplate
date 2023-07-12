c22432fcbff01b79365da1b4eb030bf4b6db6758
Tildozer <actninswitch@gmail.com>
2023-07-12 16:05:40 -0600

    got utils added onto adding the experience

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,27 +5,24 @@ import viteLogo from "/vite.svg";
 import "./index.css";
 
 function App() {
-
   return (
     <>
       <div className=" bg-slate-700 text-xl text-white">
         <div className="flex justify-center items-center">
           <a className="p-4" href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo}  alt="Vite logo" />
+            <img src={viteLogo} alt="Vite logo" />
           </a>
           <a className="p-4" href="https://react.dev" target="_blank">
-            <img src={reactLogo}  alt="React logo" />
+            <img src={reactLogo} alt="React logo" />
           </a>
         </div>
         <h1>Vite + React</h1>
-        <div >
+        <div>
           <p>
             Edit <code>src/routes/App.jsx</code> and save to test HMR
           </p>
         </div>
-        <p >
-          Click on the Vite and React logos to learn more
-        </p>
+        <p>Click on the Vite and React logos to learn more</p>
       </div>
       <canvas className="webgl"></canvas>
     </>
