ab45c2a3f73a6fea5e42ed689c521b2a138c14ee
Tildozer <actninswitch@gmail.com>
2023-07-12 19:28:50 -0600

    gonna hold up tell I learn three fiber

--- src/components/Experince.jsx
+++ src/components/Experince.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { Component, createRef, } from 'react'
+import Debug from "./Utils/Debug";
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
+import Resources from "./Utils/Resources";
+import mainSources from "./mainSources";
 
-const Experince = () => {
-  return <div></div>;
-};
+export default class Experience extends Component {
+  resize() {}
 
-export default Experince;
+  destroy() {}
+
+  update() {}
+
+  constructor(props) {
+    super();
+    // instance = this;
+    //   window.experience = this;
+
+    //  Options
+    // this.canvasRef = createRef();
+
+    this.debug = new Debug();
+    this.sizes = new Sizes(this.canvas);
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.resources = new Resources(mainSources);
+    //   this.camera = new
+  }
+
+  componentDidMount() {
+    // console.log(this.canvasRef.current);
+  }
+
+  render() {
+    return (
+           <Canvas className="bg-black">
+              {/* <canvas ref={this.canvasRef} /> */}
+              {/* <mesh geometry={<boxGeometry />}/> */}
+              
+              <mesh>
+                <boxGeometry></boxGeometry>
+              </mesh>
+          </Canvas>
+          )
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./routes/App.jsx";
 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App root={root} />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
